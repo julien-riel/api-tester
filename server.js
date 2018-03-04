ebd3e7ae22b3025943f7a9465724ddfe2bb2e468
Julien Riel <julien_riel@hotmail.com>
2018-03-04 00:49:48 -0500

    Gestion de ports

--- server.js
+++ server.js
@@ -1,28 +1,38 @@
 const express = require('express');
 const requestSA = require('superagent');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 // Configure server
 const app = express();
 app.use(bodyParser.json());
 
 // server global variables
+let port = process.env.port || process.argv[2] || 3000 ;
+console.log('Détection du port:', port);
+
+// Les valeurs par défaut de l'application
 let remoteConfig = {
     object : {
         url: "https://158fgrohl5.execute-api.ca-central-1.amazonaws.com/prod/?string=laval",
-        nbTime: 20
+        nbTime: 5,
+        sleepTime: 100
     }
 };
 let running = false;
 let callCounter = 0;
 let timerResults = [];
 
-function respond(res) {
+// ** Fonctions privées
+
+// Retourne sur la sortie http le résultat d'exécution du test
+function sendTestResult(res) {
     res.status(200)
        .json(timerResults);
 }
 
-function addTimer(counter, url, startTimer) {
+// Accumule les résultats d'exécution d'un appel au service
+// TODO : Corriger second et millisecond
+function addTestResult(counter, url, startTimer) {
     let endTimer = process.hrtime(startTimer);
     timerResults.push([
         {
@@ -34,29 +44,38 @@ function addTimer(counter, url, startTimer) {
     ]);
 }
 
-function callServiceAsync(res) {
+
+// Exécution d'un test. Appeller le url, noter les résultats et continuer
+function doOneTestAndContinue(res) {
     
     let startTimer = process.hrtime();
-    
+    let url = remoteConfig.object.url; 
     requestSA
-    .get(remoteConfig.object.url)
+    .get(url)
     .end((apiErr, apiResponse) => {
         if (apiErr) {throw apiErr;}
+        
         callCounter++;
-        addTimer(callCounter, remoteConfig.object.url, startTimer);
-        console.log('calling ', remoteConfig.object.url);
+        addTestResult(callCounter, url, startTimer);
+        console.log(callCounter + ' - calling ', url);
+        
         if (running && callCounter < remoteConfig.object.nbTime) {
-            setImmediate(() => {callServiceAsync(res);})
+            setTimeout(() => {doOneTestAndContinue(res);}, remoteConfig.object.sleepTime);
+            
         } else {
-            respond(res);
+            sendTestResult(res);
+            console.log('** Fin du test')
         }
     });
 }
 
+// ** Fonctions de l'API
+
 function start(req, res) {
+    console.log('** Début du test');
     running = true;
     callCounter = 0;
-    callServiceAsync(res)
+    doOneTestAndContinue(res);
 }
 
 function stop(req, res) {
@@ -77,10 +96,18 @@ function setConfig(req, res) {
     res.status(200).json(remoteConfig.object);
 }
 
+// Assignations des routes
 app.get('/', index);
 app.get('/start', start);
 app.get('/stop', stop);
 app.get('/config', getConfig);
 app.post('/config', setConfig);
 
-app.listen(3000, () => console.log('Server listening on port 3000!'))
+app.listen(port, () => console.log(''
+ + '  GET http://localhost:' + port + '/start pour démarrer le test.\n'
+ + '  GET http://localhost:' + port + '/config pour obtenir la configuration.\n'
+ + '  POST http://localhost:' + port + '/config pour définir la configuration.\n'
+ + '  GET http://localhost:' + port + '/stop pour arrêter le test.\n\n'
+ + 'Configuration:\n' + JSON.stringify(remoteConfig.object, null, 3) + '\n'
+ + 'Le serveur écoute sur le port ' + port
+));
